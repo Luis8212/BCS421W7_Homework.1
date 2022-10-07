ad138fbad456f8ca79db29ea3c07fde06a591d9f
LuiscLopez <luisclopez1999@gmail.com>
2022-10-07 16:09:56 -0400

    add new feature 3

--- app/src/main/java/edu/farmingdale/bcs421/bcs421_f22_w6_fragmentsdemo/MainActivity2.kt
+++ app/src/main/java/edu/farmingdale/bcs421/bcs421_f22_w6_fragmentsdemo/MainActivity2.kt
@@ -3,12 +3,17 @@ package edu.farmingdale.bcs421.bcs421_f22_w6_fragmentsdemo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.SeekBar
 import android.widget.TextView
+import java.util.*
 
 class MainActivity2 : AppCompatActivity() {
-    lateinit var mBtn1:Button
-    lateinit var mBtn2:Button
-    lateinit var mTV:TextView
+    lateinit var mBtn1: Button
+    lateinit var mBtn2: Button
+    lateinit var mTV: TextView
+    lateinit var seek: SeekBar
+    lateinit var editit: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,18 +22,29 @@ class MainActivity2 : AppCompatActivity() {
         mBtn1 = findViewById(R.id.btn1);
         mBtn2 = findViewById(R.id.btn2);
         mTV = findViewById(R.id.tv1)
+        seek = findViewById(R.id.seekBar)
+        editit = findViewById(R.id.editText01)
+
         var frgmnt01 = Fragment01()
         var frgmnt02 = Fragment02()
 
-        ///
+        seek.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
 
-        //
+                editit.setText(p1.toString())
+                mTV.setTextSize(p1.toFloat())
+            }
 
-        //
+            override fun onStartTrackingTouch(p0: SeekBar?) {
 
-        //
+            }
 
-        //
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+
+            }
+
+        })
+
 
         mBtn1.setOnClickListener{
             supportFragmentManager.beginTransaction().apply {
@@ -49,4 +65,6 @@ class MainActivity2 : AppCompatActivity() {
         var sharedPref= getSharedPreferences(MainActivity().SHAREDPREF_FILENAME, MODE_PRIVATE)
         mTV.setText(sharedPref.getString("KEY", "not forund"))
     }
+
+
 }
